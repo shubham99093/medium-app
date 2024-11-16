1cc8d4e3b9103e534d3840c14632a1b4d5db44fd
shubham99093 <shubhamchodavadiya1122@gmail.com>
2024-11-16 13:49:38 +0530

    fix: backend link

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://medium-app.onrender.com";
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://medium-app.onrender.com";
+// export const BACKEND_URL = "http://localhost:3000";
